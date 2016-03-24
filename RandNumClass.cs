dffa7bc7d0db0de4c62ccb0ebb5299d7a4a85503
Robert Currey <robertcurrey95@gmail.com>
2016-03-24 16:22:55 -0400

    Added files via upload

---
+++ RandNumClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slot_Machine
+{
+    class RandNumClass
+    {
+        private int index1;
+        private int index2;
+        private int index3;
+        
+        public RandNumClass()
+        {
+            getRandNum1(ref index1);
+            getRandNum1(ref index2);
+            getRandNum1(ref index3);
+        }
+
+        public int getRandNum1(ref int index1)
+        {
+            //INITIALIZE RNG
+            Random random = new Random();
+
+            //GET FIRST INDEX FROM RNG
+            index1 = random.Next(1, 6);
+            return index1;
+        }
+
+        public int getRandNum2(ref int index1)
+        {
+            //INITIALIZE RNG
+            Random random2 = new Random();
+
+            //GET FIRST INDEX FROM RNG
+            index2 = random2.Next(1, 4);
+
+            return index2;
+        }
+
+        public int getRandNum3(ref int index1)
+        {
+            //INITIALIZE RNG
+            Random random3 = new Random();
+
+            //GET FIRST INDEX FROM RNG
+            index3 = random3.Next(1, 5);
+
+            return index3;
+        }
+    }
+}
---
+++ SpinClass.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slot_Machine
+{
+    class SpinClass
+    {
+        private int randNum1;
+        private int randNum2;
+        private int randNum3;
+
+        private string[] picArray = new string[6];
+
+        public SpinClass()
+        {
+            //GETS RANDOM NUMBER
+            RandNumClass getNum = new RandNumClass();
+
+            randNum1 = getNum.getRandNum1(ref randNum1);
+            randNum2 = getNum.getRandNum2(ref randNum2);
+            randNum3 = getNum.getRandNum3(ref randNum3);
+
+            //CALLS SLOT IMAGE DISPLAYERS, SENDS THEM RANDOM NUMBERS
+            DisplayImage1(ref randNum1);
+            DisplayImage2(ref randNum2);
+            DisplayImage3(ref randNum3);
+        }
+
+        //SLOT IMAGE DISPLAY MODULE 1, CONTAINS ALL COMBOS
+        public void DisplayImage1(ref int randNum1)
+        {
+            int caseSwitch = randNum1;
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("Display Picture for Slot 1: A");
+                    break;
+                case 2:
+                    Console.WriteLine("Display Picture for Slot 1: B");
+                    break;
+                case 3:
+                    Console.WriteLine("Display Picture for Slot 1: C");
+                    break;
+                case 4:
+                    Console.WriteLine("Display Picture for Slot 1: D");
+                    break;
+                case 5:
+                    Console.WriteLine("Display Picture for Slot 1: E");
+                    break;
+                case 6:
+                    Console.WriteLine("Display Picture for Slot 1: F");
+                    break;
+            }
+        }
+
+        public void DisplayImage2(ref int randNum2)
+        {
+            int caseSwitch = randNum2;
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("Display Picture for Slot 2: A");
+                    break;
+                case 2:
+                    Console.WriteLine("Display Picture for Slot 2: B");
+                    break;
+                case 3:
+                    Console.WriteLine("Display Picture for Slot 2: C");
+                    break;
+                case 4:
+                    Console.WriteLine("Display Picture for Slot 2: D");
+                    break;
+                case 5:
+                    Console.WriteLine("Display Picture for Slot 2: E");
+                    break;
+                case 6:
+                    Console.WriteLine("Display Picture for Slot 2: F");
+                    break;
+            }
+        }
+
+        public void DisplayImage3(ref int randNum3)
+        {
+            int caseSwitch = randNum3;
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("Display Picture for Slot 3: A");
+                    break;
+                case 2:
+                    Console.WriteLine("Display Picture for Slot 3: B");
+                    break;
+                case 3:
+                    Console.WriteLine("Display Picture for Slot 3: C");
+                    break;
+                case 4:
+                    Console.WriteLine("Display Picture for Slot 3: D");
+                    break;
+                case 5:
+                    Console.WriteLine("Display Picture for Slot 3: E");
+                    break;
+                case 6:
+                    Console.WriteLine("Display Picture for Slot 3: F");
+                    break;
+            }
+        }
+    }
+}
